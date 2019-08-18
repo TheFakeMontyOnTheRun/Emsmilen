287244d468e8a48bb39ba44a95b2b282658b0e99
Daniel Monteiro <danielmonteiro@id.uff.br>
2019-08-18 22:30:03 +0100

    Parse function declaration types
    Also, add better printing of the node paths

--- parse-sexpression.py
+++ parse-sexpression.py
@@ -1,5 +1,21 @@
-from sexpdata import loads
+from sexpdata import loads, Symbol
 
+class TypeName:
+
+    name = None
+
+    def __init__(self, val):
+        self.name = val
+
+void_type = TypeName("void")
+
+class ValueTypeDefinition:
+    name = None
+    TypeName = None
+
+class FunctionType:
+    parameters = []
+    returnType = void_type
 
 def is_list(possible_list):
     att = dir(possible_list)
@@ -13,16 +29,84 @@ def is_list(possible_list):
 
     return False
 
+def get_atom_value(node):
+    if isinstance(node, str):
+        return node
 
-def print_list(nodes):
-    for node in nodes:
+    if isinstance(node, int):
+        return str(node)
+
+    if not is_list(node):
+        return str(node.value())
+
+    return ""
+
+
+def parse_result_declaration(param_declaration):
+
+    for node in param_declaration:
+        if param_declaration.index(node) != 0:
+            return get_atom_value(node)
+
+    return None
+
+def parse_parameter_declaration(param_declaration):
+    param_list = []
+
+    for node in param_declaration:
+        if param_declaration.index(node) != 0:
+            param_list.append(get_atom_value(node))
+
+
+    return param_list
+
+def parse_function_declaration(type_def):
+
+    func = FunctionType()
+
+    for node in type_def:
         if is_list(node) and len(node) > 0:
-            print_list(node)
-        else:
-            print(str(node))
+            if get_atom_value(node[0]) == "param":
+                func.parameters = parse_parameter_declaration(node)
+
+            if get_atom_value(node[0]) == "result":
+                func.returnType = parse_result_declaration(node)
+
+    return func
 
 
-with open('hello.dis', 'r') as myfile:
+exports = dict()
+declared_funcs = dict()
+
+
+def print_list(nodes, path):
+
+    if path == "/module/export":
+        exports[ get_atom_value( nodes[2][1] ) ] = get_atom_value( nodes[1] )
+
+
+    if path == "/module/func":
+        print("function definition:" + exports[get_atom_value( nodes[1] )] )
+
+    if path == "/module/type" and get_atom_value( nodes[2][0] ) == "func":
+        declared_funcs[get_atom_value( nodes[1] )] = parse_function_declaration(nodes[2])
+        return;
+
+
+    for node in nodes:
+
+        if nodes.index(node) != 0:
+
+            if is_list(node) and len(node) > 0:
+
+                newPath = path + "/" + get_atom_value(node[0])
+
+                print_list(node, newPath)
+            else:
+                print(path + "/" + get_atom_value(node))
+
+
+with open('simple.dis', 'r') as myfile:
     data = myfile.read()
     sexp = loads(data, nil='nop', true='1', false='0', line_comment='#')
-    print_list(sexp)
+    print_list(sexp, "/" + get_atom_value(sexp[0]))
