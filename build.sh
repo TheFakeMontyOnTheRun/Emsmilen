94aee0aeb5549d94758c3802e292805679ded83c
Daniel Monteiro <danielmonteiro@id.uff.br>
2019-08-29 23:17:15 +0100

    Add optimization pass to fix weird issues with wasm binary format

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-emcc -Oz -g -c -o $1.wasm $1.c
-~/git/emsdk/upstream/bin/wasm2wat --generate-names  ~/git/emfuzen/$1.wasm > $1.wat
+emcc -Oz -g -o $1.wasm $1.c
+~/git/emsdk/binaryen/master_64bit_binaryen/bin/wasm-opt $1.wasm -o $1_opt.wasm
+~/git/emsdk/upstream/bin/wasm2wat --generate-names  ~/git/emfuzen/$1_opt.wasm > $1.wat
 ./fix.sh ./$1.wat > ./$1.dis
